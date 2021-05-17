a5b4cbdaaa002f60655867dd23f7385fe03a3dd5
CharlesBoadu <charles.boadu@stu.ucc.edu.gh>
2021-05-17 22:36:17 +0000

    Added comments to code

--- gradeCalculator/main.cpp
+++ gradeCalculator/main.cpp
@@ -11,15 +11,20 @@ int main()
     cout <<"What is your name?"<<endl;
     cin >>name;
     cout <<"Hello "<<name<<", let's calculate your grade by entering five (5) scores"<<endl;
+
+    //Taking five integer scores from the user
     for (int i=1; i<6; i++) {
         cout <<"Enter score "<<i<<" : "<<endl;
         cin >>scores[i];
         sum += scores[i];
     }
 
+    //calculating the average
     average = sum / 5;
     cout <<"Your average is "<<average<<endl;
 
+
+    //Calculating the grade
     if (average >= 90) {
         cout <<"Grade = A";
     } else if (average >= 80 && average <= 90) {
